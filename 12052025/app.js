9b315deba213e6f317755f1171ba7881d1bf71d4
Ricardo-Zaragoza <119813324+Ricardo-Zaragoza@users.noreply.github.com>
2025-05-19 13:07:48 -0600

    TAREA_FINAAL

--- 12052025/app.js
+++ 12052025/app.js
@@ -1,29 +1,25 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Importar rutas unificadas
+const routes = require('./Rutas/routes');  // Ajusta la ruta si es necesario
+
+// Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configuración de motor de vistas y carpeta views
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// rutas
-const listRoutes = require('./Rutas/BD/list');
-const addRoutes = require('./Rutas/BD/add');
-const editarRoutes = require('./Rutas/BD/editar');
-const updateRoutes = require('./Rutas/BD/update');
-const deleteRoutes = require('./Rutas/BD/delete');
+// Usar rutas
+app.use('/', routes);
 
-app.use('/', listRoutes);
-app.use('/', addRoutes);
-app.use('/', editarRoutes);
-app.use('/', updateRoutes);
-app.use('/', deleteRoutes);
-
+// Puerto y arranque del servidor
 const port = 5721;
 app.listen(port, () => {
-    console.log(`Servidor corriendo en http://127.0.0.1:${port}`);
+  console.log(`Servidor corriendo en http://127.0.0.1:${port}`);
 });
